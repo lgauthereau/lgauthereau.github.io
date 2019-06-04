0b12d91199710aa5d70c8afad83c84c11cf54f44
Lorena Gauthereau <lorena19@gmail.com>
2019-06-04 15:08:38 -0400

    created new files

---
+++ python/codeexercise.py
@@ -0,0 +1,6 @@
+address_info = {"Name": "Lorena Gauthereau",
+"street": "1111 Street",
+"city": "Houston,", "state": "TX ", "zip": "77035"}
+print(address_info['Name'])
+print(address_info['street'])
+print(address_info['city'] + ', ' + address_info['state'] + address_info['zip'])
---
+++ python/mathproblem.py
@@ -0,0 +1,5 @@
+pages = 450
+words_per_page = 250
+number_of_pieces = 100
+chunk = (pages * words_per_page)/number_of_pieces
+print(chunk)
