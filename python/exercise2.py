c12665c923205b91c09f8aeb79c96c5d3e8f13bc
Lorena Gauthereau <lorena19@gmail.com>
2019-06-04 16:14:34 -0400

    created new file on places

--- python/exercise2.py
+++ python/exercise2.py
@@ -1,3 +1,4 @@
 data = [10, 20, 30, 40, 50]
 results = [num - 10 for num in data]
 print(results)
+#comments go here.
---
+++ python/exercise3.py
@@ -0,0 +1,3 @@
+places = ['Mexico City', 'Paris', 'Rio', 'Puerto Rico']
+results = ['I would like to visit ' + place for place in places]
+print(results)
